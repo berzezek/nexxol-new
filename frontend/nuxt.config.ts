a51b1decd247b0bcaf799bca321b4daadff0fd88
ShamsBams <shamsbams@gmail.com>
2023-02-06 19:02:50 +0500

    add some login

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,23 +1,20 @@
-import {setAbsoluteSqliteDatabaseUrlForPrisma} from './prisma/utils'
+import { setAbsoluteSqliteDatabaseUrlForPrisma } from "./prisma/utils";
 
-setAbsoluteSqliteDatabaseUrlForPrisma()
+setAbsoluteSqliteDatabaseUrlForPrisma();
 
 export default defineNuxtConfig({
-  runtimeConfig: {
-    version: '0.0.1'
-  },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-svgo',
-    '@huntersofbook/naive-ui-nuxt',
-    '@pinia/nuxt',
-  ],
-  extends: ['@sidebase/core'],
-  typescript: {
-    shim: false
-  },
-  css: [
-    '~/assets/css/input.css',
-  ],
-
-})
+	runtimeConfig: {
+		version: "0.0.1",
+	},
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"nuxt-svgo",
+		"@huntersofbook/naive-ui-nuxt",
+		"@pinia/nuxt",
+	],
+	extends: ["@sidebase/core"],
+	typescript: {
+		shim: false,
+	},
+	css: ["~/assets/css/input.css"],
+});
